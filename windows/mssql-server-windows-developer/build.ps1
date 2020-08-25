222f0125d5f37ba6ba3675e6ac49dd1ae9ad5e5f
Dewald Slabbert <dewald@k2.com>
2020-08-25 11:08:26 +0200

    update build for tagging and escape char

--- windows/mssql-server-windows-developer/build.ps1
+++ windows/mssql-server-windows-developer/build.ps1
@@ -1,6 +1,8 @@
 docker build -t `
-scth/mssql-server-windows-2019-developer:latest `
+scth/mssql-server-windows-2019-developer:10.0.17763.1397 `
     --no-cache `
     .
 
 docker rmi $(docker images -f "dangling=true" -q) -f
+
+docker tag scth/mssql-server-windows-2019-developer:10.0.17763.1397 scth/mssql-server-windows-2019-developer:latest
